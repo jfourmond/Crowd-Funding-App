2515ea34cc0c1ef364c86e3fb797d19556ef8a0d
jfourmond <jerome@fourmond.fr>
2016-03-14 11:32:30 +0100

    Création fichier .jsp & Servlets
    + logon.jsp
    	-> Formulaire de connexion
    
    + signup.jsqp
    	-> Formulaire d'inscription
    	
    + LogOn.java
    	-> Servlets pour logon.jsp
    	
    + SignUp.java
    	-> Servlets pour sigup.jsp
    	
    * Renommade Accueil.java -> Home.java

--- Crowd Funding App/src/fr/m1info/rv2j/servlets/Home.java
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/Home.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Accueil extends HttpServlet {
+public class Home extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
---
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/LogOn.java
@@ -0,0 +1,7 @@
+package fr.m1info.rv2j.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+public class LogOn extends HttpServlet {
+
+}
