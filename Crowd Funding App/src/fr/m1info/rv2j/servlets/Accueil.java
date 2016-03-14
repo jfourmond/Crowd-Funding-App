4be95681945ebeec49ad5b7d68b97bdacccb21b3
jfourmond <jerome@fourmond.fr>
2016-03-14 09:41:57 +0100

    Servlet Accueil

---
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/Accueil.java
@@ -0,0 +1,20 @@
+package fr.m1info.rv2j.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Accueil extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public final static String view = "/accueil.jsp";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(view).forward(req, resp);
+	}
+}
