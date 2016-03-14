1925145e2ac25af4b7e9e73562b21f4615e9bd54
jfourmond <jerome@fourmond.fr>
2016-03-14 11:41:10 +0100

    Fonctionnel

--- Crowd Funding App/src/fr/m1info/rv2j/servlets/LogOn.java
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/LogOn.java
@@ -1,7 +1,21 @@
 package fr.m1info.rv2j.servlets;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class LogOn extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
+	public final static String view = "/WEB-INF/logon.jsp";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(view).forward(req, resp);
+	}
+	
 }
--- Crowd Funding App/src/fr/m1info/rv2j/servlets/SignUp.java
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/SignUp.java
@@ -1,5 +1,20 @@
 package fr.m1info.rv2j.servlets;
 
-public class SignUp {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SignUp extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+public final static String view = "/WEB-INF/signup.jsp";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(view).forward(req, resp);
+	}
 }
