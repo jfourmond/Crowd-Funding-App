cd92cc492752e370d34a8d227c443ebd875751f0
jfourmond <jerome@fourmond.fr>
2016-03-14 11:05:58 +0100

    Classe User

--- Crowd Funding App/src/fr/m1info/rv2j/beans/User.java
+++ Crowd Funding App/src/fr/m1info/rv2j/beans/User.java
@@ -1,22 +1,36 @@
 package fr.m1info.rv2j.beans;
 
+import java.sql.Date;
+
+/**
+ * Représentation d'un Utilisateur
+ */
 public class User {
-	/* champs */
+	private int id;
 	private String name;
 	private String pw;
 	private String email;
+	private Date inscription_date;
 	
-	/* getters */
+	/*	GETTERS	*/
+	public int getId() { return id; }
+	
 	public String getName() { return name; }
 	
 	public String getPassword() { return pw; }
 	
 	public String getEmail() { return email; }
 	
-	/* setters */
-	public void setName(String name) { this.name = name; };
+	public Date getInscriptionDate() { return inscription_date; }
 	
-	public void setPassword(String pw) { this.pw = pw; };
+	/*	SETTERS	*/
+	public void setID(int id) { this.id = id; }
+	
+	public void setName(String name) { this.name = name; }
+	
+	public void setPassword(String pw) { this.pw = pw; }
 	
 	public void setEmail(String email) { this.email = email; }
+	
+	public void setInscriptionDate(Date date) { this.inscription_date = date; }
 }
