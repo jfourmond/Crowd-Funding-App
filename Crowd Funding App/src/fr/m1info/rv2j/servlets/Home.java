0484382c1ec46a087e4bc12b1544b98838cf60c4
jfourmond <jerome@fourmond.fr>
2016-03-14 14:18:44 +0100

    Affichage Accueil avec CSS sur root application

--- Crowd Funding App/src/fr/m1info/rv2j/servlets/Home.java
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/Home.java
@@ -11,7 +11,7 @@ public class Home extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public final static String view = "/WEB-INF/accueil.jsp";
+	public final static String view = "/accueil.jsp";
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
