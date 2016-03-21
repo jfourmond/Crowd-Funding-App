b7b3123639e980a08e8d27820632fadd27278ee1
vdeleeuw <valerian.deleeuw@gmail.com>
2016-03-21 09:52:31 +0100

    DAO project

---
+++ Crowd Funding App/src/fr/m1info/rv2j/dao/ProjectDAO.java
@@ -0,0 +1,16 @@
+package fr.m1info.rv2j.dao;
+
+import java.util.List;
+import fr.m1info.rv2j.beans.Project;
+
+public interface ProjectDAO {
+	void create(Project project) throws DAOException;
+	
+	void update(String id, Project project) throws DAOException;
+	
+	Project findByID(String id) throws DAOException;
+	
+	List<Project> getAllProjects() throws DAOException;
+	
+	void deleteByID(String id) throws DAOException;
+}
