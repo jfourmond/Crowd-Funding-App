ef09ae5c76886378222903b0eb3a2ad39611b834
jdefaye <johan.defaye@hotmail.fr>
2016-03-21 10:17:28 +0100

    Ajout de ProjectEdit

--- Crowd Funding App/src/fr/m1info/rv2j/servlets/ProjectEdit.java
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/ProjectEdit.java
@@ -1,41 +1,68 @@
 package fr.m1info.rv2j.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class ProjectEdit
- */
-@WebServlet("/ProjectEdit")
+import fr.m1info.rv2j.beans.User;
+import fr.m1info.rv2j.dao.DAOFactory;
+import fr.m1info.rv2j.dao.ProjectDAO;
+import fr.m1info.rv2j.forms.AdminUserEdition;
+
 public class ProjectEdit extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ProjectEdit() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public final static String CONF_DAO_FACTORY = "daofactory";
+	
+	public final static String view_form = "/WEB-INF/admin/project_edit.jsp";
+	public final static String path_success = "project_management";
+	
+	public final static String SESSION = "session_user";
+	
+	public final static String USER = "user";
+	public final static String FORM = "form";
+	
+	private ProjectDAO projectDAO;
+	
+	@Override
+	public void init() throws ServletException {
+		projectDAO = ((DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY)).getProjectDao();
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+		User user_session = (User) session.getAttribute(SESSION);
+		User user;
+		if(user_session == null || user_session.getRightLevel() != 2) {
+			resp.sendError(401);
+		} else {
+			user = (User) req.getAttribute(USER);
+			req.setAttribute(USER, user);
+			this.getServletContext().getRequestDispatcher(view_form).forward(req, resp);
+		}
 	}
-
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user;
+		AdminUserEdition form = new AdminUserEdition(userDAO);
+		
+		user = form.editUser(req);
+		
+		if (form.getErrors().isEmpty())
+			resp.sendRedirect(resp.encodeRedirectURL(path_success)); 
+			// this.getServletContext().getRequestDispatcher(view_success).forward(req, resp);
+		else {
+			req.setAttribute(USER, user);
+			req.setAttribute(FORM, form);
+			this.getServletContext().getRequestDispatcher(view_form).forward(req, resp);
+		}
+	}
+	
 }
