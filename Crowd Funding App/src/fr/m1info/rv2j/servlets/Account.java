d49780354eba11b1f1229dbd12ea2453a83a64c4
jfourmond <jerome@fourmond.fr>
2016-03-21 12:01:19 +0100

    Ajout traitement projets dans le servlet Account

--- Crowd Funding App/src/fr/m1info/rv2j/servlets/Account.java
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/Account.java
@@ -1,6 +1,7 @@
 package fr.m1info.rv2j.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,21 +9,41 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import fr.m1info.rv2j.beans.Project;
+import fr.m1info.rv2j.beans.User;
+import fr.m1info.rv2j.dao.DAOFactory;
+import fr.m1info.rv2j.dao.ProjectDAO;
+
 public class Account extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
+	public final static String CONF_DAO_FACTORY = "daofactory";
+	
 	public final static String view = "/WEB-INF/account.jsp";
 	public final static String SESSION = "session_user";
 	
+	public final static String PROJECTS = "projects";
+	
+	ProjectDAO projectDAO;
+	
+	@Override
+	public void init() throws ServletException {
+		projectDAO = ((DAOFactory) getServletContext().getAttribute(CONF_DAO_FACTORY)).getProjectDao();
+	}
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		HttpSession session = req.getSession();
-		if(session.getAttribute(SESSION) == null)
+		User user_session = (User) session.getAttribute(SESSION);
+		
+		List<Project> projects;
+		if(user_session == null)
 			resp.sendError(401);
 		else {
+			projects = projectDAO.findByAuthorID(String.valueOf(user_session.getID()));
 			// session.setAttribute(SESSION, user);
+			req.setAttribute(PROJECTS, projects);
 			this.getServletContext().getRequestDispatcher(view).forward(req, resp);
 		}
 	}
