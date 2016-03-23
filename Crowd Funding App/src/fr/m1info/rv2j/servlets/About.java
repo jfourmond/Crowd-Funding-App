6c9fc111ea78df148718bb26d8d5e87ac6523804
vdeleeuw <valerian.deleeuw@gmail.com>
2016-03-23 10:50:14 +0100

    ajout de la page about

---
+++ Crowd Funding App/src/fr/m1info/rv2j/servlets/About.java
@@ -0,0 +1,21 @@
+package fr.m1info.rv2j.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class About extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	public final static String view = "/WEB-INF/about.jsp";
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher(view).forward(req, resp);
+	}
+}
+
